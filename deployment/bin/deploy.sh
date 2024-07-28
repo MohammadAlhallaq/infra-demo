2cbd75c9f711da76229ce73a23e878033e796b33
MohammadAlhallaq <mohammad.alhallaq1@gmail.com>
2024-07-28 16:13:15 +0300

    init

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -13,8 +13,8 @@ git config --global --add safe.directory $PROJECT_DIR
 
 # the project has not been cloned yet (first deploy)
 if [ ! -d $PROJECT_DIR"/.git" ]; then
-  GIT_SSH_COMMAND='ssh -i /home/.ssh/id_rsa -o IdentitiesOnly=yes' git clone git@github.com:MohammadAlhallaq/infra-demo.git
+  GIT_SSH_COMMAND='ssh -i /home/mohammad/.ssh/id_rsa -o IdentitiesOnly=yes' git clone git@github.com:MohammadAlhallaq/infra-demo.git
 else
-  GIT_SSH_COMMAND='ssh -i /home/.ssh/id_rsa -o IdentitiesOnly=yes' git pull
+  GIT_SSH_COMMAND='ssh -i /home/mohammad/.ssh/id_rsa -o IdentitiesOnly=yes' git pull
 fi
 
