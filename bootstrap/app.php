f78a4a68aa9053129f519f1a6620b835e0c6fb4c
MohammadAlhallaq <mohammad.alhallaq1@gmail.com>
2026-06-10 14:04:07 +0300

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,5 +18,4 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-        
     })->create();
